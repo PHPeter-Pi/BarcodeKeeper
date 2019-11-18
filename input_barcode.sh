f525ed9996632bc34d0e1a78924b2e2a5ae1a318
KEINOS <github@keinos.com>
2019-11-18 12:15:37 +0900

    fix: ファイル名に日付が入らない

--- input_barcode.sh
+++ input_barcode.sh
@@ -19,13 +19,13 @@ PATH_FILE_ENV="${PATH_DIR_CURR}/${NAME_FILE_ENV}"
 #echo 'Source path:' $PATH_FILE_ENV
 export $(grep -v '^#' $PATH_FILE_ENV | xargs)
 
-# 保存先のパス指定
-NAME_FILE_DATA="barcode_${ID_DATE}.csv"
-PATH_DIR_DATA="/data"
-PATH_FILE_DATA="${PATH_DIR_DATA}/${NAME_FILE_DATA}"
-
 # ファイル名に使われる ID
 ID_DATE=$(date +'%Y%m%d')
+
+# 保存先のパス指定
+NAME_FILE_DATA="barcode_${ID_DATE:-offline}.csv"
+PATH_DIR_DATA="/data"
+PATH_FILE_DATA="${PATH_DIR_DATA}/${NAME_FILE_DATA}"
 
 # 処理を停止する読み取りコード
 CODE_STOP="${CODE_STOP:-STOP}"
